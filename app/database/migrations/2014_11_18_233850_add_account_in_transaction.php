773dda6a1c5c1c9b2e5f0042a07a8eb0c7316870
miro <vcasinha@gmail.com>
2014-11-19 00:46:43 +0100

    add account_id in transactions migration

--- app/database/migrations/2014_11_18_233850_add_account_in_transaction.php
+++ app/database/migrations/2014_11_18_233850_add_account_in_transaction.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAccountTransaction extends Migration {
+class AddAccountInTransaction extends Migration {
 
 	/**
 	 * Run the migrations.
